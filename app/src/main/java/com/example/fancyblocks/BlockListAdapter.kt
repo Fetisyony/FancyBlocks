dc5274cb23673ac593584f79ca129259f1605a86
Fetisov Igor <fetisyony@gmail.com>
2024-10-27 16:06:09 +0300

    Implemented two different layouts for landscape and vertical orientations. Implemented state restore. Improved UI across the app

--- app/src/main/java/com/example/fancyblocks/BlockListAdapter.kt
+++ app/src/main/java/com/example/fancyblocks/BlockListAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.fancyblocks
 
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 
-class BlockListAdapter() : RecyclerView.Adapter<BlockListViewHolder>() {
-    private var items = ArrayList<Int>()
-
+class BlockListAdapter(private val context: Context, private var items: MutableList<Int>) : RecyclerView.Adapter<BlockListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlockListViewHolder {
         return BlockListViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.block_view, parent, false)
@@ -19,9 +21,14 @@ class BlockListAdapter() : RecyclerView.Adapter<BlockListViewHolder>() {
 
     override fun onBindViewHolder(holder: BlockListViewHolder, position: Int) {
         holder.bind(items[position])
+
+        if (position % 2 == 0)
+            holder.itemView.findViewById<MaterialCardView>(R.id.card).setCardBackgroundColor(ContextCompat.getColor(context, R.color.blue))
+        else
+            holder.itemView.findViewById<MaterialCardView>(R.id.card).setCardBackgroundColor(ContextCompat.getColor(context, R.color.red))
     }
 
-    fun setItems(list: ArrayList<Int>) {
+    fun setItems(list: MutableList<Int>) {
         items = list
         notifyItemRangeChanged(0, list.size)
     }
--- app/src/main/java/com/example/fancyblocks/MainActivity.kt
+++ app/src/main/java/com/example/fancyblocks/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -12,23 +13,36 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var fab: FloatingActionButton
 
+    private var numbers = mutableListOf(1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState != null) {
+            val restoredList = savedInstanceState.getIntegerArrayList("NUMBER_LIST")
+            if (restoredList != null) {
+                numbers = restoredList.toMutableList()
+            }
+        }
 
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.rv)
         fab = findViewById(R.id.fab)
-        val numbers: ArrayList<Int> = ArrayList<Int>()
-        val adapter = BlockListAdapter()
-        recyclerView.adapter = adapter
 
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        val adapter = BlockListAdapter(this, numbers)
+        recyclerView.adapter = adapter
 
         fab.setOnClickListener {
             val newNumber = (numbers.size + 1)
 
-            numbers.add(newNumber)
             adapter.addItem(newNumber)
+            recyclerView.smoothScrollToPosition(numbers.size - 1);
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putIntegerArrayList("NUMBER_LIST", ArrayList(numbers))
     }
 }
